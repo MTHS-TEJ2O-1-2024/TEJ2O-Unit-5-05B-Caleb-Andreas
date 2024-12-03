00f5f706d85ee911f5f68acf8ddcafbd57e5fcee
Caleb-Andreas <caleb.andreas@mths.ca>
2024-12-03 16:24:55 +0000

    started code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,18 +1,19 @@
 """
 Created by: Caleb Andreas
-Created on: Sep 2020
+Created on: Nov 2024
 This program makes the lights on the robotbit a traffic light.
 """
 
 from microbit import *
-import neopixel 
+import neopixel
 
-# variable
-neopixelStrip: neopixel.Strip = null
+# variables
+neopixelStrip: neopixel.Strip = 0
 
-neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
+while True:
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
